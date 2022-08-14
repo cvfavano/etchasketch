273b012de19b4da0b3a72e55fbc7aff4c7cc03fe
Christa Favano <crstav@gmail.com>
2022-08-14 00:22:16 -0400

    Add parseInt when accepting prompt

--- script.js
+++ script.js
@@ -2,33 +2,42 @@ const container = document.querySelector('.container');
 
 
 function createBoard(number,  callback) {
-    
     const div = document.createElement('div');
     container.appendChild(div).classList.add('square');
-
+    
     let i = 0;
     do{
-        createSquares(number);
+        createSquares( number);
         i++;
     }
     while(i < number);
     
+    styleSquares(number);
     callback();
 }
 
-function calculateWidth(number) {
-    return squareWidth = 700 / (number+2);
-}
 
-function createSquares(num){
+
+function createSquares( num){
+
     const copiedDiv = document.querySelector('.square');
-    let squareWidth = calculateWidth(num);
+
+    
     for (let i = 0; i < num; i++){
         const clone = copiedDiv.cloneNode(false);
         container.appendChild(clone);
-        clone.style.width = squareWidth + 'px';
-        clone.style.height = squareWidth + 'px';
     }
+}
+
+function styleSquares(num){
+    let squareWidth = 700/(num+2);
+  
+    const copiedDiv = document.querySelectorAll('.square');
+   
+    copiedDiv.forEach(div  => {
+        div.style.width = squareWidth + 'px';
+        div.style.height = squareWidth + 'px';
+     } );
 }
 
 function changeColor(){
@@ -37,5 +46,24 @@ function changeColor(){
     targets.forEach(target => target.addEventListener("mouseover", (event) => 
        event.target.style.backgroundColor = '#000'));
 }   
+
+document.querySelector('.changeGrid').addEventListener('click', changePixelSize);
+
+function clearBoard() {
+    document.querySelector('.container').innerHTML = '';
+}
+
+function changePixelSize() {
+   let px = prompt('Enter number up 100:');
+
+   if(px > 100) {
+    prompt('Please enter less than or equalt o 100:');
+   }
+   pixelNum = parseInt(px)
+   clearBoard();
+   createBoard(pixelNum,  changeColor); 
+
+}
    
-createBoard(16,  changeColor);    
+createBoard(16,  changeColor);  
+  
